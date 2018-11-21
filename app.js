3109d674ae0b2458bf717515fba868d16372c861
nehaejaz <neha.ejaz@icloud.com>
2018-11-22 03:48:17 +0500

    Sending a post request

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http=require('http');
+const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head>My first page</head>');
+        res.write('<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head>My first page</head>');
+    res.write('<body><h1>Neha is the best!</h1></body>');
+    res.write('</html>');
+    res.end();
+
 
 });
 
-server.listen(3000);
+server.listen(3000)
